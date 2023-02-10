76ee61e6b08ab35d81107911bebbbbb273434e5e
poon <poon15112@gmail.com>
2023-02-10 16:48:03 +0700

    lap17_1 completed

--- lab17_1.cpp
+++ lab17_1.cpp
@@ -32,3 +32,36 @@ int main(){
 	findColSum(dPtr,sum2,N,M); 
 	showData(sum2,1,M);
 }
+void showData(double *dPtr,int N,int M){
+    cout << fixed << setprecision(2);
+    for(int i = 0; i < N*M; i++){
+        cout << *(dPtr+i);
+        if(i%M == M-1) cout << endl;
+        else cout << " ";
+    }
+}
+void randData(double *data,int N,int M){
+    for(int i = 0; i < N*M; i++){
+        *(data+i) = (rand()%101)*0.01;
+    }
+}void findRowSum(const double *dPtr,double *sum1,int N,int M){
+    
+    for(int i = 0; i < N; i++){
+        double sum = 0;
+        for(int j = 0; j < M; j++){
+            sum += *(dPtr+j+i*M);
+        }
+        *(sum1+i) = sum;
+    }
+
+}
+void findColSum(const double *dPtr,double *sum2,int N,int M){
+     for(int i = 0; i < M; i++){
+        double sum = 0;
+        for(int j = 0; j < N; j++){
+            sum += *(dPtr+i+j*M);
+        }
+        *(sum2+i) = sum;
+    }
+
+}
